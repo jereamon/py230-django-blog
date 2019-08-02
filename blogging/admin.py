6cd99dcb19a30bfd2e62c13014cc32d7c1b00bc9
Jeremy <jereamon@gmail.com>
2019-08-01 17:29:11 -0700

    Assignment_07 complete. Categories can now be added from the post itself.

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
-# Register your models here.
-admin.site.register(Post)
-admin.site.register(Category)
 
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    # inlines = [
+    #     CategoryInline,
+    # ]
+    exclude = ('posts',)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInline
+    ]
+
+
+# admin.site.register(Post)
+# admin.site.register(Category)
